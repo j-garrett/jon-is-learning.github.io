833d7b681b9a1578b71be15b1cfdb8b7d7aa8ac1
jon-is-learning <jonathan.garrett@gmail.com>
2015-04-29 13:34:55 -0700

    Twitter Card and Facebook Open Graph Meta added

--- bookList/js/custom.js
+++ bookList/js/custom.js
@@ -8,3 +8,5 @@
         lessLink: '<a href="#"><em>...Read less</em></a>',
         moreLink: '<a href="#"><em>Read more...</em></a>'
     });
+    //twitter share button
+    !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
