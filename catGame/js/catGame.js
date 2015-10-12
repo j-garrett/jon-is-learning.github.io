9447be019df89b1121e2176887ca7f2ab7efe5c0
jon-is-learning <jonathan.garrett@gmail.com>
2015-10-12 16:40:39 -0700

    Testing flexbox & settings modal

--- catGame/js/catGame.js
+++ catGame/js/catGame.js
@@ -1,4 +1,23 @@
-//function to call when Start Game button is pushed
-function catGame(){
+//create catGame object to constrain scope (Global Abatement described in JS: the Good Parts)
+var catGame = {};
+//create catGame properties & methods:
 
-}
+//track the cuteness modifier
+catGame.cuteness = 0;
+
+//track and display remaining game time with this variable.
+catGame.gameTimer = 0;
+
+//track prey divs created and caught
+catGame.preyCreated = 0;
+catGame.preyKilled = 0;
+
+//method to start the game. may have to nest other functions and whatnot inside?
+catGame.startGame = function(){};
+
+//timing event that sets interval and calls the appropriate functions each second.
+//appropriate functions: decrement gameTimer, chance to poach, add prey divs to HTML, etc.
+catGame.timer = function(){};
+
+//controls the playing field. adding the divs and animations to make the hunting more... interesting.
+catGame.mapField = function(){};
