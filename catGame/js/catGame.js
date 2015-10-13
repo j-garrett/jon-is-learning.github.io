2ecc4c1c690da373f8ea97894c4b335528ce35c5
jon-is-learning <jonathan.garrett@gmail.com>
2015-10-13 16:27:28 -0700

    Bones of HTML
    Modal and playing screen uploaded. Fixed issue with horizontal
    scrolling.
    
    Next: start adding in the JS snippets from codepen!

--- catGame/js/catGame.js
+++ catGame/js/catGame.js
@@ -13,7 +13,9 @@ catGame.preyCreated = 0;
 catGame.preyKilled = 0;
 
 //method to start the game. may have to nest other functions and whatnot inside?
-catGame.startGame = function(){};
+catGame.startGame = function(event){
+    alert('You\'re adorable. And hopefully patient... We\'re not ready!');
+};
 
 //timing event that sets interval and calls the appropriate functions each second.
 //appropriate functions: decrement gameTimer, chance to poach, add prey divs to HTML, etc.
@@ -21,3 +23,15 @@ catGame.timer = function(){};
 
 //controls the playing field. adding the divs and animations to make the hunting more... interesting.
 catGame.mapField = function(){};
+
+
+//WORK WITH HTML PAGE:
+//set DOM variables
+var startButton = document.getElementById('startGameButton');
+//etc.
+
+//add event listeners to DOM
+//Compare the below event listener with the method used on codepen http://codepen.io/jon-is-learning/pen/xwgpXd?editors=001
+//the Hunt and Kill Mice code pen puts listener on parent element as listener. is this better?
+startButton.addEventListener("click", catGame.startGame, false);
+
