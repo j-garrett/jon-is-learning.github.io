27310dab6c2f1630e3c9d02f8bf80a99a9da6f16
Jon Garrett <jonathan.garrett@gmail.com>
2015-12-06 09:42:05 -0800

    Call Numbers to Column as Link
    Added .js to remove old call numbers from body text and add improved
    link versions to list. (list items are THEN turned into tables so we may
    need to play with delimiter later)

--- videographies/conversion.js
+++ videographies/conversion.js
@@ -4,36 +4,47 @@ $(document).ready(function(){
   var callNumberSearch = new RegExp(/([vV]ideo\/C|DVD|v\/c|sound\/d|s\/d)(\s?)(x|z|(999)?)\s?:?(\s?)(\d{1,4})/ig);
   //turn DOM into array so you can iterate over it
   var elems = document.getElementsByTagName('dd');
-  //convert NodeList to an Array
+  //convert NodeList of all <dd> elements to an Array so we can iterate through
   var elemArray = jQuery.makeArray(elems);
   //for each tag, grab element's HTML so you can replace call numbers with new html
   //iterate the code for each <dd> to find all call numbers
-  $.each(elemArray,function(){
+  $.each(elemArray,function(index, value){
     //create variable for current node's html
     var html = $(this).html();
+    //create empty array that the for loop will add all call number links to
+    //scope requires it be here
+    var elemArray = [];
+    //make looping variable out here to save slightly on memory usage
+    var i = '';
     //Search for call number using regex and turn result into usable variable
     //test if there was a match before attempting the rest. this saves from erroring out if no match.
     if (html.match(callNumberSearch)){
       var callNumbersArray = html.match(callNumberSearch);
       //callNumber is smooshing the array into string. we need to perform this business below for EACH element
       //tried to do .each, but it didn't work properly. Take a closer look at how array is formed
-      console.log(callNumbersArray);
-      //the tricky part may be putting it BACK into the DOM after doing the work on each bit.
-      //Look closer at Array.prototype.join() for building the array back up and returning to DOM after iterating
-      var callNumber = html.match(callNumberSearch).toString();
-      //Swap out that slash for %2F so the Video/C call numbers work in query Url
-      var callNumberReplace1 = callNumber.replace('/', '%2F');
-      //Swap out the space for a + for the same reasons
-      var callNumberReplace2 = callNumberReplace1.replace(' ', '+');
-      //Concatenate into URL
-      var callNumberUrl = 'http://oskicat.berkeley.edu/search~S1?/e' + callNumberReplace2;
-      //create replacement element with proper linking
-      var newElem = '<a href="' + callNumberUrl + '">' + callNumber + '</a>';
-      //first parameter searches for match, second parameter is a string that replaces it
-      //$& inserts the matched text
-      //THE URL NEEDS TO BE ENCODED SO THE SPACE BECOMES A PLUS SIGN
-      //THIS WILL BE IN FUNCTION THAT IS LOOPED OVER FOR EACH <dd> using toArray.each()
-      $(this).html(html.replace(callNumberSearch, newElem));
+      for(i = 0; i < callNumbersArray.length ; i++){
+        //the tricky part may be putting it BACK into the DOM after doing the work on each bit.
+        //Look closer at Array.prototype.join() for building the array back up and returning to DOM after iterating
+        var callNumber = callNumbersArray[i].toString();
+        //Swap out that slash for %2F so the Video/C call numbers work in query Url
+        var callNumberReplace1 = callNumber.replace('/', '%2F');
+        //Swap out the space for a + for the same reasons
+        var callNumberReplace2 = callNumberReplace1.replace(' ', '+');
+        //Concatenate into URL
+        var callNumberUrl = 'http://oskicat.berkeley.edu/search~S1?/e' + callNumberReplace2;
+        //create replacement element with proper linking
+        var newElem = '<a href="' + callNumberUrl + '">' + callNumber + '</a>';
+        //add created link to array that will be pushed to DOM after loop
+        elemArray.push(newElem);
+        //join array so it's nice and pretty
+        //THIS WILL NEED TO CHANGE TO PROPER DELIMITER?
+        var newCallElem = elemArray.join(', ');
+      }   
+      //delete call numbers in <dd>
+      $(this).html(html.replace(callNumberSearch, ''));
+      //once array is built we can THEN push it back into DOM
+      //we will add it as a new element so it can be properly linked and delimited for csv transfer
+      $(this).append('</dt><dt>'+newCallElem+'</dt>');
     }
   });
 
